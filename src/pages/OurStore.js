dd50683bf88b19628445b23d6c3c438fc0b9a8e7
Anil Kumar B <anilkumarb12102gmail.com>
2024-02-03 16:44:08 +0530

    BreadCrumb.js new file: MetaD.js OurStore.js add a new page OurStore

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import BreadCrumb from '../components/BreadCrumb'
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/MetaD';
 
 const OurStore = () => {
   return (
     <>
+    <Meta title={'Our Store'}/>
     <BreadCrumb title="Our Store"/>
     </>
   )
 }
 
-export default OurStore
+export default OurStore;
