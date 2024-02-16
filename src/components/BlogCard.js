2da8fd1e7025dc259b8ee2718c06d3ee499c4991
Anil Kumar B <anilkumarb12102gmail.com>
2024-02-16 19:07:25 +0530

    completed blogs and contact pages

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 
 const BlogCard = () => {
   return (
-    <div className="col-3">
       <div className="blog-card">
         <div className="card-image">
-          <img src="images/blog-1.jpg" className="img-fluid" alt="blog"></img>
+          <img src="images/blog-1.jpg" className="img-fluid w-100" alt="blog"></img>
         </div>
         <div className="blog-content">
           <p className="date"> {new Date().toDateString()}</p>
@@ -20,7 +19,6 @@ const BlogCard = () => {
           </Link>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -337,10 +337,20 @@ const Home = () => {
             <div className="col-12">
               <h3 className="section-heading"> Our Latest Blogs</h3>
             </div>
+           <div className="row">
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
+           </div>
           </div>
         </div>
       </section>
